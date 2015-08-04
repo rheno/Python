7a764fc244454fd0d41b8a9625aa2b9236f3f281
rhenobudiasa <rhenobudiasa@yahoo.com>
2015-08-05 06:46:54 +0700

    Read Simple SOAP Response

--- ReadXML/readSOAP.py
+++ ReadXML/readSOAP.py
@@ -1,7 +1,14 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('soap.xml')    
 
-#print tree.find('.//{http://tempuri.org/wsSalesQuotation/Service1}LoginResult').text
-#print tree.find('.//{http://www.example.org/stock}Price').text
-for p in tree.findall('.//{http://tempuri.org/wsSalesQuotation/Service1}LoginResult'):
-	print p.text
+tree = ET.parse('simple-soap.xml')
+root = tree.findall('.//{http://lalala.org/}GetResult')
+
+# Single row iterate. For example : ID column value
+"""
+for row in tree.findall('.//{http://schemas.datacontract.org/SomeFormat}ID'):
+	print row.text
+"""
+
+# Multiple row iterate.
+for row in root[0]:
+	print "ID = ",row[0].text,", Date ",row[1].text,", Divsion ",row[2].text,", Product ",row[3].text,", Model_Alias ",row[4].text,", Mobile No ",row[5].text,", Sales ",row[6].text
