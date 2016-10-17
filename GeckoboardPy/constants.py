18ea3a3e5df69b7a29e75f6b04fb0b50c2d0b934
rheno.manggala <rheno.manggala@uangteman.com>
2016-10-17 18:48:09 +0700

    Add constants.py and edit geckoboard_api.py

---
+++ GeckoboardPy/constants.py
@@ -0,0 +1,7 @@
+# see https://app.geckoboard.com/account/details to see your API_KEY
+
+URL = "https://api.geckoboard.com"
+ 
+API_KEY = "API_KEY"
+
+PASSWORD = "PASSWORD"
--- GeckoboardPy/geckoboard_api.py
+++ GeckoboardPy/geckoboard_api.py
@@ -4,36 +4,108 @@
 import urllib2, base64
 import json
 import httplib
+import constants
 
-# Put your url datasets, API_KEY, and Password (Not used in my case)
-URL = "https://api.geckoboard.com/datasets/sales.gross/data"
-API_KEY = "API_KEY"
-PASSWORD = ""
+class PyGeckoboard:
+		
+	def auth(self, prefix):
+		return self.request(prefix, "", "GET")
+		
+	def create_dataset(self, prefix, *args, **kwargs):
+		data = {}
+                unique_by = []
 
-# Python Dictionary (You can modify this to get data from database or other sources)
-pydict = {'data': [{"timestamp" : "2016-01-01T12:00:00Z", "amount" : 219}, {"timestamp" : "2016-01-02T12:00:00Z", "amount" : 900}]}
+                data = kwargs
+                for arg in args:
+                        unique_by.append(arg)
 
-# Change dictionary to array
-jsondata = json.dumps(pydict)
+                data["unique_by"] = unique_by
+		return self.request(prefix, data, "PUT")
 
-# Make request
-request = urllib2.Request(URL, jsondata)
-base64string = base64.b64encode('%s:%s' % (API_KEY, PASSWORD))
+	def replace_dataset(self, prefix, *args):
 
-# Setup Header
-request.add_header("Authorization", "Basic %s" % base64string)   
-request.add_header("Content-Type", "application/json")
+		field = []
+		data = {}
+		for arg in args :
+                	field.append(arg)
+
+		data["data"] = field
+
+		return self.request(prefix, data, "PUT")
+
+	def append_dataset(self, prefix, *args):
+
+                field = []
+                data = {}
+                for arg in args :
+                        field.append(arg)
+
+                data["data"] = field
+
+                return self.request(prefix, data, "POST")
 
 
-# Request and get the response here
-try :
-    response = urllib2.urlopen(request)
-    print "Success : "+response.read()
-except urllib2.HTTPError, e:
-    print 'HTTPError = %d'%e.code
-except urllib2.URLError, e:
-    print 'URLError = '.str(e.reason)
-except httplib.HTTPException, e:
-    print 'HTTPException'
-except Exception:
-    print "Parsing Response or Others Error"
+	def delete_dataset(self, prefix):
+		return self.request(prefix, "", "DELETE")
+		
+
+	
+	def request(self, prefix, data, method):
+	
+		# Get response
+		response = ""
+		
+		# Change dictionary to array
+		jsondata = json.dumps(data)
+
+		request = ""
+
+		# Make request
+		if method == "POST" or method == "PUT" :
+			request = urllib2.Request(constants.URL + "%s"% (prefix), jsondata)
+		elif method == "GET" or method == "DELETE" :			
+			request = urllib2.Request(constants.URL + "%s"% (prefix))
+
+		base64string = base64.b64encode('%s:%s' % (constants.API_KEY, constants.PASSWORD))
+
+		# Setup Header
+		request.add_header("Authorization", "Basic %s" % base64string)
+		request.add_header("Content-Type", "application/json")
+		request.get_method = lambda:"%s"%method
+
+
+		# Request and get the response here
+		try :
+    			resp = urllib2.urlopen(request)
+    			response = "Success : "+resp.read()
+		except urllib2.HTTPError, e:
+    			response = 'HTTPError = %d' % e.code
+		except urllib2.URLError, e:
+    			response = 'URLError = %s' % str(e.reason)
+		except httplib.HTTPException, e:
+    			response = 'HTTPException'
+		except Exception:
+			response = "Parsing Response or Others Error"
+		
+		return response
+
+
+p = PyGeckoboard() 	
+
+# print p.create_dataset("/datasets/iseng.bisa", 
+#		"tanggal",
+#		fields=dict(
+#		jumlah=dict(type="number",name="Jumlah"), 
+#		tanggal=dict(type="date", name="Tanggal"))
+#		)
+
+# print p.delete_dataset("/datasets/iseng.bisa")
+
+print p.append_dataset("/datasets/sales.gross/data", 
+			dict(timestamp="2016-01-01T12:00:00Z", amount=877), 
+			dict(timestamp="2016-01-29T12:00:00Z", amount=655) 
+			)
+
+
+
+
